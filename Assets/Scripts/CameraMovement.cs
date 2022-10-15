47392d40816ff85e249db4d200b6e44edb192426
aleedca <acerdas1701@estudiantec.cr>
2022-10-15 08:50:02 -0600

    characters sprites

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -7,15 +7,10 @@ public class CameraMovement : MonoBehaviour
     public float followSpeed = 4f;
     public float yOffset = -1f;
     private Transform player1, player2;
-
-    void Start()
-    {
-        player = GameObject.Find("Player").transform;
-    }
-
+    
     void Update()
     {
         Vector3 newPosPlayer1 = new Vector3(player1.position.x, player1.position.y + yOffset, -10f); //player x y position
-        transform.position = Vector3.Slerp(tranform.position, newPosPlayer1, followSpeed * Time.deltatime);
+        transform.position = Vector3.Slerp(transform.position, newPosPlayer1, followSpeed * Time.deltaTime);
     }
 }
